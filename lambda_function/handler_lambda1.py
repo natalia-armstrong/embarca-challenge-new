2a51de23f768405d5d3109778617a4a6806804b6
Natalia Armstrong <156014605+natalia-armstrong@users.noreply.github.com>
2025-02-03 17:27:03 -0300

    Alteracao lambdas style

--- lambda_function/handler_lambda1.py
+++ lambda_function/handler_lambda1.py
@@ -6,41 +6,31 @@ from datetime import datetime
 
 def main(event, context):
     try:
-        # Carregar os links do arquivo link.yml
         links = event.get('links', {})
-
-        # Nome do bucket S3 (usando .get() para evitar KeyError)
         s3_bucket = os.environ.get('S3_BUCKET', None)
         
         if not s3_bucket:
             raise ValueError("S3_BUCKET environment variable is missing or invalid.")
 
-        # Lista para armazenar os resultados
         results = []
 
-        # Iterar sobre os links e processá-los
         for name, url in links.items():
-            # Nome do arquivo a ser salvo no S3, usando timestamp para nome exclusivo
             file_name = f"csv_files/{name}_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
             
-            # Baixar o arquivo CSV
             response = requests.get(url)
             
             if response.status_code != 200:
                 raise Exception(f"Failed to download file from {url}. Status Code: {response.status_code}")
 
-            # Salvar o CSV no S3
             s3 = boto3.client('s3')
             s3.put_object(Bucket=s3_bucket, Key=file_name, Body=response.content)
             
-            # Adicionar a URL S3 ao resultado
             results.append({
                 'csv_url': f"s3://{s3_bucket}/{file_name}",
                 'file_name': file_name,
                 'original_link': url
             })
         
-        # Retornar os resultados para a próxima função
         return {
             'files': results
         }
--- lambda_function/handler_lambda2.py
+++ lambda_function/handler_lambda2.py
@@ -4,82 +4,57 @@ from sqlalchemy import create_engine
 from datetime import datetime
 from io import StringIO
 
-# Função para processar o CSV
 def process_csv(bucket_name, file_key):
-    """
-    Baixa o arquivo CSV do S3 e o carrega em um DataFrame pandas.
-    """
-    # Baixar o arquivo CSV do S3
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket=bucket_name, Key=file_key)
     file_content = response['Body'].read().decode('utf-8')
 
-    # Usar pandas para ler o CSV diretamente do conteúdo
     csv_data = StringIO(file_content)
-    df = pd.read_csv(csv_data, delimiter=";")  # O separador é ";"
+    df = pd.read_csv(csv_data, delimiter=";") 
 
     return df
 
-# Função para calcular o número de mortes por veículo
 def calculate_deaths(df):
     """
     Calcula o número total de mortes e por tipo de veículo.
     """
     vehicle_columns = ["automovel", "bicicleta", "caminhao", "moto", "onibus"]
     
-    # Somar os mortos por veículo
     vehicle_deaths = df[vehicle_columns].sum()
 
-    # Somar o total de mortos
     total_deaths = df["mortos"].sum()
 
     print(f"Total de mortes: {total_deaths}")
     print(f"Mortos por veículo: {vehicle_deaths}")
 
-    # Criar uma lista de dicionários com os dados para salvar no banco
     insert_data = []
     for vehicle, deaths in vehicle_deaths.items():
         insert_data.append({
             "created_at": datetime.now(),
-            "road_name": df["trecho"].iloc[0],  # Usando o nome do primeiro trecho como exemplo
+            "road_name": df["trecho"].iloc[0], 
             "vehicle": vehicle,
             "number_deaths": deaths
         })
     
     return insert_data
 
-# Função para salvar os dados no banco de dados
 def save_to_db(insert_data, db_url):
-    """
-    Salva os dados calculados no banco de dados.
-    """
-    # Criar conexão com o banco de dados usando SQLAlchemy
-    engine = create_engine(db_url)  # db_url deve ter o formato: 'postgresql://usuario:senha@host:porta/banco'
 
-    # Convertendo a lista de dicionários para um DataFrame do pandas
+    engine = create_engine(db_url) 
+
     insert_df = pd.DataFrame(insert_data)
-
-    # Inserir os dados no banco (supondo que já exista uma tabela chamada "accident_deaths")
     insert_df.to_sql('accident_deaths', con=engine, if_exists='append', index=False)
 
-# Função principal para orquestrar o processo
 def process_and_save(bucket_name, file_key, db_url):
-    """
-    Função principal que orquestra as operações de processamento e salvamento dos dados.
-    """
-    # Processar o CSV
+
     df = process_csv(bucket_name, file_key)
     
-    # Calcular as mortes
     insert_data = calculate_deaths(df)
     
-    # Salvar no banco de dados
     save_to_db(insert_data, db_url)
 
-# Exemplo de uso
-bucket_name = 'step-functions-challenge-dev-bucket'  # Substitua pelo seu bucket
-file_key = 'csv_files/Via Araucaria_20250203102246.csv'  # Caminho do arquivo no S3
-db_url = 'postgresql://username:password@localhost:5432/mydatabase'  # Substitua pela URL do seu banco de dados
+bucket_name = 'step-functions-challenge-dev-bucket' 
+file_key = 'csv_files/Via Araucaria_20250203102246.csv' 
+db_url = 'postgresql://username:password@localhost:5432/mydatabase'
 
-# Executar o processo
 process_and_save(bucket_name, file_key, db_url)
